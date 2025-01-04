ad76a09569fa9d24a33168234d818b77943bf6f9
Jonas Erhart <jonas.erhart@student.uibk.ac.at>
2025-01-05 00:40:08 +0100

    config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -19,18 +19,19 @@ alias git-cap="git commit --amend --no-edit && git push -f"
 
 function fish_greeting
 	set greetings \
-		"What do you want!?" \
-		"Welcome to where your dreams come to meet syntax errors" \
-		"Another day, another poorly indented piece of code..." \
-		"I've seen your commit history. We need to talk." \
-		"Could you knock next time?" \
-		"Are you here to check on some of your bugs?" \
-		"Other people go outside sometimes, you know?" \
-    "Ready to judge your variable naming conventions"
+		"󱚟  What do you want!?" \
+		"󰚩  I've seen your commit history. We need to talk." \
+		"󱚝  [WARN] Could you knock next time?" \
+		"󱚣  Are you here to check on some of your bugs?" \
+		"󰚩  Other people go outside sometimes, you know?" \
+    "󱚣  Ready to judge your variable naming conventions" \
+    "󰚩  [INFO] go fmt has opinions about your life choices"
 
 	set chosen_msg (random)"%"(count $greetings)
 	set chosen_msg $greetings[(math $chosen_msg"+1")]
 
 	# Output it to the console
-	printf (set_color F90)"%s\n" $chosen_msg
+  printf (set_color B1A2CA)"-----------------------------------------------------------\n"
+	printf "  %s\n" $chosen_msg
+  printf "-----------------------------------------------------------\n"
 end
