42218734c60d908622ec52d9d30df0bf792631f8
Jonas Erhart <jonas.erhart@student.uibk.ac.at>
2025-01-05 00:27:49 +0100

    add fish config and variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -17,3 +17,20 @@ alias k="kubectl"
 alias lsl="ls -l"
 alias git-cap="git commit --amend --no-edit && git push -f"
 
+function fish_greeting
+	set greetings \
+		"What do you want!?" \
+		"Welcome to where your dreams come to meet syntax errors" \
+		"Another day, another poorly indented piece of code..." \
+		"I've seen your commit history. We need to talk." \
+		"Could you knock next time?" \
+		"Are you here to check on some of your bugs?" \
+		"Other people go outside sometimes, you know?" \
+    "Ready to judge your variable naming conventions"
+
+	set chosen_msg (random)"%"(count $greetings)
+	set chosen_msg $greetings[(math $chosen_msg"+1")]
+
+	# Output it to the console
+	printf (set_color F90)"%s\n" $chosen_msg
+end
