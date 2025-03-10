d0d87056e0d2f36683ae904055cac0b34de19eaa
Jonas Erhart <jonas.erhart@dynatrace.com>
2025-03-10 15:13:59 +0100

    chore: addec cmp, ref: NOISSUE

--- .config/fish/completions/ref.fish
+++ .config/fish/completions/ref.fish
@@ -5,7 +5,7 @@ function __fish_ref_dynamic_completions
     for line in $tasks_and_projects
       set -l task $(echo $line | cut -d ':' -f 1)
       set -l proj $(echo $line | cut -d ':' -f 2)
-      complete -c ref -a "$task" -d "$proj"
+      complete -c ref -x -a "$task" -d "$proj"
     end
 
   end
