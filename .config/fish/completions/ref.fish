351a21915e0c6385f74fbfed5604fcc6beba441f
Jonas Erhart <jonas.erhart@dynatrace.com>
2025-03-10 14:54:04 +0100

    config changes

--- .config/fish/completions/ref.fish
+++ .config/fish/completions/ref.fish
@@ -1,4 +1,15 @@
-if type -q task
-  complete -c ref -x -a '(task _unique ref)'
+function __fish_ref_dynamic_completions
+  if type -q task
+    set tasks_and_projects $(task export | jq -r '[.[] | select(has("ref"))] | unique_by(.ref) | .[] | "\(.ref):\(.project)"')
+
+    for line in $tasks_and_projects
+      set -l task $(echo $line | cut -d ':' -f 1)
+      set -l proj $(echo $line | cut -d ':' -f 2)
+      complete -c ref -a "$task" -d "$proj"
+    end
+
+  end
 end
+
+__fish_ref_dynamic_completions
 
