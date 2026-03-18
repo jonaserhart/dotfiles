0b091454ddce663f620d104819ca48e9c64711ed
Jonas Erhart <jonas.erhart@dynatrace.com>
2026-03-18 12:56:50 +0100

    fix golangci-lint

--- .config/nvim/lua/plugins/config/conform.lua
+++ .config/nvim/lua/plugins/config/conform.lua
@@ -5,20 +5,20 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				-- first, run golang-ci-lint then run gopls lsp formatter in every case
-				go = { "golangci-lint", "gopls" },
+				go = { "golangci" },
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 			},
+      formatters = {
+        ["golangci"] = {
+          command = "golangci-lint",
+          args = { "fmt", "--stdin"},
+          stdin = true,
+        },
+      },
 			format_on_save = {
-				timeout_ms = 500,
+				timeout_ms = 2000,
 				lsp_format = "fallback",
 			},
-		})
-
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			pattern = "*",
-			callback = function(args)
-				require("conform").format({ bufnr = args.buf })
-			end,
 		})
 	end,
 }
